36e3daf8c364cd8f49e9516baca89291bdaddec3
mb753 <224386536+mb753@users.noreply.github.com>
2025-09-12 17:25:33 +0300

    Add database schema and initial data

---
+++ init.sql
@@ -0,0 +1,25 @@
+DELETE FROM users;
+DELETE FROM recipes;
+DELETE FROM ingredients;
+DELETE FROM instructions;
+
+INSERT INTO users (username) VALUES ('Mestarikokki');
+
+INSERT INTO recipes (title, user_id) VALUES ('Mikrokaurapuuro', 1);
+INSERT INTO ingredients (ingredient, recipe_id) VALUES ('1 dl pikakaurahiutaleita', 1);
+INSERT INTO ingredients (ingredient, recipe_id) VALUES ('2 dl vettä tai maitoa', 1);
+INSERT INTO ingredients (ingredient, recipe_id) VALUES ('halutessasi ripaus suolaa', 1);
+INSERT INTO instructions (instruction, recipe_id) VALUES ('Sekoita ainekset ja keitä mikrossa noin 2 minuuttia.', 1);
+INSERT INTO instructions (instruction, recipe_id) VALUES ('Nauti esimerkiksi marjojen kera.', 1);
+
+INSERT INTO recipes (title, user_id) VALUES ('Kiireisen päivän linssikeitto', 1);
+INSERT INTO ingredients (ingredient, recipe_id) VALUES ('200 g kuivattuja punaisia linssejä', 2);
+INSERT INTO ingredients (ingredient, recipe_id) VALUES ('1 purkki yrttitomaattimurskaa', 2);
+INSERT INTO ingredients (ingredient, recipe_id) VALUES ('1 sipuli', 2);
+INSERT INTO ingredients (ingredient, recipe_id) VALUES ('1 liemikuutio', 2);
+INSERT INTO ingredients (ingredient, recipe_id) VALUES ('sopiva määrä vettä', 2);
+INSERT INTO ingredients (ingredient, recipe_id) VALUES ('tilkka rypsiöljyä', 2);
+INSERT INTO instructions (instruction, recipe_id) VALUES ('Huuhtele linssit siivilässä.', 2);
+INSERT INTO instructions (instruction, recipe_id) VALUES ('Pilko sipuli ja kuullota se tilkassa öljyä kattilan pohjalla.', 2);
+INSERT INTO instructions (instruction, recipe_id) VALUES ('Lisää muut ainekset ja keitä noin 20 minuuttia.', 2);
+INSERT INTO instructions (instruction, recipe_id) VALUES ('Halutessasi viimeistele valmis annos tuoreilla yrteillä.', 2);
---
+++ schema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    username TEXT UNIQUE,
+    password_hash TEXT
+);
+
+CREATE TABLE recipes (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE ingredients (
+    id INTEGER PRIMARY KEY,
+    ingredient TEXT,
+    recipe_id INTEGER REFERENCES recipes
+);
+
+CREATE TABLE instructions (
+    id INTEGER PRIMARY KEY,
+    instruction TEXT,
+    recipe_id INTEGER REFERENCES recipes
+);
