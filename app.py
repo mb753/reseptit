07787ff68b3766606510c79c386930d9ba1c3af7
mb753 <224386536+mb753@users.noreply.github.com>
2025-09-12 17:32:38 +0300

    Modify app to show data from database

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask import render_template
+import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", message="Tänne tulee reseptisovellus.")
+    recipes = db.query("""SELECT r.title, u.username
+                       FROM recipes r, users u
+                       WHERE r.user_id = u.id
+                       """)
+    return render_template("index.html", message="Reseptisovellus on rakenteilla.", recipes=recipes)
