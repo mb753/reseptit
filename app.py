4780b75db059821aadb0822f17f5c85703fc9dfe
mb753 <224386536+mb753@users.noreply.github.com>
2025-09-12 20:50:55 +0300

    Add recipe pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import abort, render_template
 import db
 
 app = Flask(__name__)
@@ -11,3 +11,14 @@ def index():
                        WHERE r.user_id = u.id
                        """)
     return render_template("index.html", message="Reseptisovellus on rakenteilla.", recipes=recipes)
+
+@app.route("/recipe/<int:recipe_id>")
+def recipe(recipe_id):
+    recipe = db.query("""SELECT r.title, u.username
+                      FROM recipes r, users u
+                      WHERE r.id = ? AND r.user_id = u.id
+                      """, [recipe_id])
+    recipe = recipe[0] if recipe else abort(404)
+    ingredients = db.query("SELECT ingredient FROM ingredients WHERE recipe_id = ?", [recipe_id])
+    instructions = db.query("SELECT instruction FROM instructions WHERE recipe_id = ?", [recipe_id])
+    return render_template("recipe.html", message="Reseptisovellus on rakenteilla.", recipe=recipe, ingredients=ingredients, instructions=instructions)
