696115655ac007e53414f5c65beeb428f0912494
mb753 <224386536+mb753@users.noreply.github.com>
2025-09-13 16:12:36 +0300

    Add registration and login/logout functionality

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from flask import abort, render_template, redirect, request
-import db
+from flask import abort, render_template, redirect, request, session
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+import config, db
 
 app = Flask(__name__)
+app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
@@ -30,3 +33,49 @@ def recipe(recipe_id):
     comments = db.query("SELECT c.comment, c.user_id, u.username FROM comments c, users u WHERE c.recipe_id = ? AND u.id = c.user_id", [recipe_id])
 
     return render_template("recipe.html", message="Reseptisovellus on rakenteilla.", recipe=recipe, ingredients=ingredients, instructions=instructions, comments=comments)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("register.html", result="VIRHE: salasanat eivät ole samat")
+        if password1 == "":
+            return render_template("register.html", result="VIRHE: salasana on pakollinen")
+        password_hash = generate_password_hash(password1)
+
+        try:
+            sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+            db.execute(sql, [username, password_hash])
+        except sqlite3.IntegrityError:
+            return render_template("register.html", result="VIRHE: tunnus on jo varattu")
+        return render_template("register.html", result="Tunnus luotu")
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        sql = "SELECT password_hash FROM users WHERE username = ?"
+        password_hash = db.query(sql, [username])[0][0]
+
+        if check_password_hash(password_hash, password):
+            session["username"] = username
+            return redirect("/")
+        else:
+            return render_template("login.html", result="VIRHE: väärä tunnus tai salasana")
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
