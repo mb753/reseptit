0615b48a9ca81c261810d5fc7577cfb25e721f48
mb753 <224386536+mb753@users.noreply.github.com>
2025-09-13 13:00:41 +0300

    Add commenting functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import abort, render_template
+from flask import abort, render_template, redirect, request
 import db
 
 app = Flask(__name__)
@@ -12,13 +12,21 @@ def index():
                        """)
     return render_template("index.html", message="Reseptisovellus on rakenteilla.", recipes=recipes)
 
-@app.route("/recipe/<int:recipe_id>")
+@app.route("/recipe/<int:recipe_id>", methods=["GET", "POST"])
 def recipe(recipe_id):
-    recipe = db.query("""SELECT r.title, u.username
+    recipe = db.query("""SELECT r.title, r.id, u.username
                       FROM recipes r, users u
                       WHERE r.id = ? AND r.user_id = u.id
                       """, [recipe_id])
     recipe = recipe[0] if recipe else abort(404)
+
+    if request.method == "POST":
+        comment = request.form["comment"]
+        db.execute("INSERT INTO comments (comment, recipe_id, user_id) VALUES (?, ?, ?)", [comment, recipe_id, 3])
+        return redirect("/recipe/" + str(recipe_id))
+
     ingredients = db.query("SELECT ingredient FROM ingredients WHERE recipe_id = ?", [recipe_id])
     instructions = db.query("SELECT instruction FROM instructions WHERE recipe_id = ?", [recipe_id])
-    return render_template("recipe.html", message="Reseptisovellus on rakenteilla.", recipe=recipe, ingredients=ingredients, instructions=instructions)
+    comments = db.query("SELECT c.comment, c.user_id, u.username FROM comments c, users u WHERE c.recipe_id = ? AND u.id = c.user_id", [recipe_id])
+
+    return render_template("recipe.html", message="Reseptisovellus on rakenteilla.", recipe=recipe, ingredients=ingredients, instructions=instructions, comments=comments)
